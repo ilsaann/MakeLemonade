fc58e866b5e8aa4862d5db9a29289cc17e260bb3
Ilsa <hamptonilsa96@gmail.com>
2022-08-02 17:42:26 -0400

    got the quotes up onto the page and relatively formatted- need to stylise

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const $getQuote = $(".getQuote");
 
 $getQuote.click((event) => {
   $.get("https://type.fit/api/quotes", (data) => {
+    const $displayQuote = $(".displayQuote");
+    $displayQuote.html("");
     const quotes = JSON.parse(data);
     console.log(quotes);
     const count = quotes.length - 1;
@@ -11,6 +13,11 @@ $getQuote.click((event) => {
       `http://www.randomnumberapi.com/api/v1.0/random?min=0&max=${count}&count=1`,
       (number) => {
         const n = number[0];
+        const Quote = quotes[n].text;
+        const Author = quotes[n].author;
+        const $display = $("<p></p>").html(`${Quote}<br>${Author}`);
+        $displayQuote.append($display);
+
         console.log(quotes[n]);
       }
     );
